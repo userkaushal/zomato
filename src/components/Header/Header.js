746f81276b31cc266d65805e18ce8dfa73df7d13
userkaushal <kaushamahida@gmail.com>
2024-03-28 12:18:48 +0530

    Some Changes Responsive
    Some Changes to add Responsiveness and added cards section.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,35 +1,89 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 import iconsm from "../../logos/iconsm.svg"; // Path to your image
 import ddown from "../../logos/ddown.svg";
 import abc from "../../logos/search.svg";
+import mobsign from "../../logos/mobsign.svg";
+import getappmob from "../../logos/getappmob.svg";
 
 function Header() {
+
+  // For Responsive Mobile View
+
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 767);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 600);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+
+  // For Dropdown Menu Input
+
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [selectedItem, setSelectedItem] = useState('');
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+    setShowDropdown(false);
+  };
+
   return (
     <div>
       <div className="header">
         <div className="bg-hero">
           <img src="https://b.zmtcdn.com/web_assets/81f3ff974d82520780078ba1cfbd453a1583259680.png" />
-          <div className="nav">
-            <ul>
-              <li>
-                <Link to="/">Get the App</Link>
-              </li>
-              <li>
-                <Link to="/">Investor Relations</Link>
-              </li>
-              <li>
-                <Link to="/">Add restaurant</Link>
-              </li>
-              <li>
-                <Link to="/">Log in</Link>
-              </li>
-              <li>
-                <Link to="/">Sign up</Link>
-              </li>
-            </ul>
-          </div>
+
+
+          {isSmallScreen ? (
+            <div className="nav nav-mob">
+              <span><Link to="https://www.zomato.com/mobile"><img src={getappmob} />&nbsp;Get the App</Link></span>
+              <span>
+                <div className="logo-mob">
+                  <Link to='/'>
+                    <img src={mobsign} />
+                  </Link>
+                </div>
+              </span>
+            </div>
+
+          ) : (
+
+            <div className="nav">
+              <ul>
+                <li>
+                  <Link to="/">Get the App</Link>
+                </li>
+                <li>
+                  <Link to="/">Investor Relations</Link>
+                </li>
+                <li>
+                  <Link to="/">Add restaurant</Link>
+                </li>
+                <li>
+                  <Link to="/">Log in</Link>
+                </li>
+                <li>
+                  <Link to="/">Sign up</Link>
+                </li>
+              </ul>
+            </div>
+
+          )}
+
           <div className="wrapper">
             <div className="logoimg">
               <img
@@ -38,7 +92,7 @@ function Header() {
               />
             </div>
             <h1>
-              Discover the best food & drinks in <span>Ahmedabad</span>
+              Discover the best food & drinks in <span className="next-line">Ahmedabad</span>
             </h1>
             <div className="searchcontainer">
               <div className="mergeddiv">
@@ -47,11 +101,21 @@ function Header() {
                     <i>
                       <img src={iconsm} alt="location" />
                     </i>
-                    <input type="text" placeholder="Ahmedabad" />
+                    <input type="text" placeholder="Ahmedabad" value={selectedItem}
+                      onClick={toggleDropdown} />
                     <i className="ddown">
                       <img src={ddown} alt="location" />
                     </i>
                   </div>
+                  {showDropdown && (
+                    <div>
+                      <ul>
+                        <li onClick={() => handleItemClick('Item 1')}>Item 1</li>
+                        <li onClick={() => handleItemClick('Item 2')}>Item 2</li>
+                        <li onClick={() => handleItemClick('Item 3')}>Item 3</li>
+                      </ul>
+                    </div>
+                  )}
                 </div>
                 <div className="indicate"></div>
                 <div className="right">
--- src/components/main/Cardstwo.js
+++ src/components/main/Cardstwo.js
@@ -8,11 +8,27 @@ function Cardstwo() {
       <div className='flex'>
         <div className='merges'>
           <div className='card'>
-            <div className='inrflex'>
-              <Link to='' >
-                <img src='zomato/src/logos/orderonline.avif' alt='OrderOnline img' width={100}/>
-              </Link>
-            </div>
+            <Link href='/'>
+              <div className='inrflex'>
+                <img src="https://b.zmtcdn.com/webFrontend/e5b8785c257af2a7f354f1addaf37e4e1647364814.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*" alt="Image" className="card-image" />
+                <div className="card-text">
+                  <p className='title'>Order Online</p>
+                  <p className='subtitle'>Stay home and order to your doorstep</p>
+                </div>
+              </div>
+            </Link>
+          </div>
+
+          <div className='card'>
+            <Link href='/'>
+              <div className='inrflex'>
+                <img src="https://b.zmtcdn.com/webFrontend/d026b357feb0d63c997549f6398da8cc1647364915.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*" alt="Image" className="card-image" />
+                <div className="card-text">
+                  <p className='title'>Dining</p>
+                  <p className='subtitle'>View the city's favourite dining venues</p>
+                </div>
+              </div>
+            </Link>
           </div>
         </div>
 
